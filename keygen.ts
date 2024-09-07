545e834bbad736c14324f349c8fe13696fd95757
Andre <dat.andre.on.juno@gmail.com>
2024-09-07 12:18:28 +0100

    first commit

---
+++ keygen.ts
@@ -0,0 +1,9 @@
+import { Keypair } from "@solana/web3.js";
+// Now we should create a keypair, like this:
+
+// Generate a new keypair
+let kp = Keypair.generate();
+console.log(`You have generate a new Solana wallet: ${kp.publicKey.toBase58()}`);
+console.log(`You have generate a new Solana wallet: ${kp.publicKey.toString()}`);
+
+console.log(`Solana wallet secret key: ${kp.secretKey}`);
